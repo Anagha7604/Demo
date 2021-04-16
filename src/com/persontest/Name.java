1723c56b3382df7adf01e0e1e834c866bcdfc34b
Anagha7604 <anaghachandroth@gmail.com>
2021-04-16 10:29:37 +0530

    test commit

---
+++ src/com/persontest/Name.java
@@ -0,0 +1,4 @@
+package com.persontest;
+
+public class Name {
+}
--- src/com/persontest/Person.java
+++ src/com/persontest/Person.java
@@ -1,9 +1,15 @@
 package com.persontest;
 
-
+import com.persontest.Name;
 public class Person {
+
+    private Name personName;
 
     public String helloworld() {
         return "Hello World";
+    }
+
+    public Name name(){
+        return personName;
     }
 }
