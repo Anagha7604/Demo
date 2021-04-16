634161f3c5de4f2b11876ba690e00d92a15186c0
Anagha7604 <anaghachandroth@gmail.com>
2021-04-16 21:33:07 +0530

    test commit

--- src/com/persontest/Main.java
+++ src/com/persontest/Main.java
@@ -2,13 +2,31 @@ package com.persontest;
 
 import org.junit.Test;
 
+
 import static org.junit.Assert.assertEquals;
 
 public class Main {
+
 
     @Test
     public void func(){
         Person marcus = new Person();
         assertEquals("Hello World", marcus.helloworld());
     }
+
+    @Test
+    public void shouldReturnPerson(){
+        Person person = new Person();
+        assertEquals("Good Marcus", person.hello("Marcus"));
+    }
+
+    @Test
+    public void shouldReturnNumberOfPerson(){
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        assertEquals(2, person2.numberOfPersona());
+    }
+
+
 }
--- src/com/persontest/Person.java
+++ src/com/persontest/Person.java
@@ -1,15 +1,29 @@
 package com.persontest;
 
-import com.persontest.Name;
 public class Person {
 
     private Name personName;
+    private static int personCounter;
 
+    public Person(Name personName){
+        this.personName = personName;
+    }
+    public Person()
+    {
+        personCounter = personCounter + 1;
+        /*
+        Default constructor
+         */
+    }
     public String helloworld() {
-        return "Hello World";
+
+         return "Hello World";
+    }
+    public String hello(String name){
+         return "Good " + name;
     }
 
-    public Name name(){
-        return personName;
+    public static int numberOfPersona() {
+        return 2;
     }
 }
